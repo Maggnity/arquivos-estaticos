13d2a6e35e6b2a9f97f418fa49e8bb51b541c816
Fernando <mag.nando07@gmail.com>
2022-05-12 18:31:03 -0300

    First commit

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,14 @@
+import styles from './Cardapio.module.scss';
+import { ReactComponent as Logo } from '../../assets/img/logo.svg';
+
+function Cardapio() {
+    return (
+        <main>
+            <nav className={styles.menu}>
+                <Logo />
+            </nav>
+        </main>
+    )
+}
+
+export default Cardapio
