cb110885acde9e3ed6972c7635f757841f5d6e0e
Fernando <mag.nando07@gmail.com>
2022-05-13 16:32:59 -0300

    Aula 6.2 - Fazendo o buscador e filtro funcionar

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,37 @@
 import cardapio from './itens.json';
 import Item from './Item';
 import styles from './Itens.module.scss'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-function Itens() {
+interface Props {
+    busca: string,
+    filtro: number | null,
+    ordenador: string
+}
+
+function Itens(props: Props) {
+    const [lista, setLista] = useState(cardapio);
+    const { busca, filtro} = props;
+    
+    function testaBusca(title:string) {
+        const regex = new RegExp(busca, 'i'); /* i significa case insensitive */
+        return regex.test(title);
+    }
+
+    function testaFiltro(id:number) {
+        if (filtro !== null) return filtro === id;
+        return true;
+    }
+
+    useEffect(() => {
+        const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+        setLista(novaLista);
+    },[busca, filtro]);
+    
     return (
         <div className={styles.itens}>
-            {cardapio.map((item) => (
+            {lista.map((item) => (
                 <Item 
                     key={item.id}
                     {...item}
