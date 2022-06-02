679c30490592d31248fe65226c726492558d3132
Fernando Vinícius <mag.nando07@gmail.com>
2022-06-02 16:03:08 -0300

    Aula 4.2 - Router - Reaproveitando styles

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,15 +1,16 @@
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss'; 
+import stylesTema from 'styles/Tema.module.scss';
 
 function Inicio() {
     let pratosRecomendados = [...cardapio];
     pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
     return (
         <section>
-            <h3 className={styles.titulo}>
+            <h3 className={stylesTema.titulo}>
                 Recomendações da cozinha
             </h3>
-            <div className={styles.recomendado}>
+            <div className={styles.recomendados}>
                 {pratosRecomendados.map(item => (
                     <div key={item.id} className={styles.recomendado}>
                         <div className={styles.recomendado__imagem}>
